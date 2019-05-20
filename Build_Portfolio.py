822488ef7e70a9ce9ae74ed14d1e554488d9b4f8
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2019-05-20 16:01:01 +0100

    Update Build_Portfolio.py

--- Build_Portfolio.py
+++ Build_Portfolio.py
@@ -1,11 +1,17 @@
 #######################################################################################################################################
 # 
-# This function creates a portfolio given a factor, size, funds and a specific date, for Euro Stoxx 600 Stocks. 
-# Stocks can be changed by specifying their associated tag. 
+# This function creates a portfolio of European stocks based on sensitivities to a given factor. 
 # 
+# Inputs: 
+#         factor - 'factor' (e.g. 'ADXY')
+#         size - number of stocks to be in resulting portfolio (e.g. 10)
+#         date - 'date' (e.g. '2019-05-17')
+#         term - 'term' (e.g. 'Long Term')
+#
 #######################################################################################################################################
 
-def get_portfolio(factor,size,funds,date):
+
+def get_portfolio(factor,size,date,term):
 
     FACTOR_sensitivity = []
     names = []
@@ -13,11 +19,11 @@ def get_portfolio(factor,size,funds,date):
 
     # Get ID's of the Euro Stoxx 600 Stocks.
     # Stocks can be changed by specifying another stock's tag. 
-    euro_stoxx_600 = [x.name for x in api_instance.get_models(tags="STOXX Europe 600")][::2]
+    euro_stoxx_600 = [x.name for x in api_instance.get_models(tags="STOXX Europe 600")][::20]
 
-    for i in euro_stoxx_600:
+    for asset in euro_stoxx_600:
 
-        sensitivity = api_instance.get_model_sensitivities(model=i,date_from=date,date_to=date,term = 'Long Term')
+        sensitivity = api_instance.get_model_sensitivities(model=asset,date_from=date,date_to=date,term = term)
 
         df_sensitivities = pandas.DataFrame()
 
@@ -27,29 +33,22 @@ def get_portfolio(factor,size,funds,date):
             for data in sensitivity[date]:
                 df_sensitivities[str(data['driver_short_name'])]=[data['sensitivity']]
 
-            top10 = df_sensitivities.transpose().nlargest(10,0)
+            top5 = abs(df_sensitivities).T.nlargest(5,0)
             Factor_sens = float(df_sensitivities[factor])
 
-            if factor in top10.index and Factor_sens > 0:
+            if factor in top5.index:
                 FACTOR_sensitivity.append(Factor_sens)
-                names.append(api_instance.get_model(model=i).name)
-                position = top10.index.tolist().index(factor)+1
+                names.append(asset)
+                position = top5.index.tolist().index(factor)+1
                 POSITION.append(position)
 
-    portfolio_size = size
-
     df_factor_sensit = pandas.DataFrame({'Name':names,'Position':POSITION,factor+' Sensitivity':FACTOR_sensitivity})
-    portfolio = df_factor_sensit.nlargest(portfolio_size,str(factor)+' Sensitivity')
+    portfolio = df_factor_sensit.nlargest(size,str(factor)+' Sensitivity')
 
     sw = 1/(portfolio['Position']+2)
-    summ = sum(sw)
-    Weights = sw/summ
-    position_values = [funds*x for x in Weights]
-    factor_exposure = [a*b/100 for a,b in zip(portfolio[factor+' Sensitivity'],position_values)]
+    Weights = sw/sw.sum()
 
     portfolio = portfolio.drop(['Position'], axis=1)
     portfolio.insert(1,'Weight',Weights)
-    portfolio.insert(2,'Position Value',position_values)
-    portfolio[factor+' Exposure'] = factor_exposure
     
     return portfolio
