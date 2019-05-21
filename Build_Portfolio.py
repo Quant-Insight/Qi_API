dd567bbe43e459d0b1e93c1b20698729e39b1f70
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2019-05-21 13:18:59 +0100

    Update Build_Portfolio.py

--- Build_Portfolio.py
+++ Build_Portfolio.py
@@ -8,6 +8,19 @@
 #         date - 'date' (e.g. '2019-05-17')
 #         term - 'term' (e.g. 'Long Term')
 #
+# Output: 
+#         dataframe with the following columns:
+#               * Position - dataframe index. 
+#               * Name - top 5 factor drivers' names. 
+#               * Weight - weights of the top 5 factor drivers.
+#               * Factor Sensitivity - Factor top 5 drivers' sensitivity.
+#               * e.g.
+#                       Name | Weight   | ADXY Sensitivity
+#                  2	LAND | 0.203390 | 0.09368
+#                  1	KGH  | 0.237288 | 0.07690
+#                  0	GPOR | 0.203390 | 0.05345
+#                  3	TIT  | 0.355932 | 0.04598
+#
 #######################################################################################################################################
 
 
@@ -32,7 +45,7 @@ def get_portfolio(factor,size,date,term):
 
             for data in sensitivity[date]:
                 df_sensitivities[str(data['driver_short_name'])]=[data['sensitivity']]
-
+            # We want the top 5 drivers in absolute terms. 
             top5 = abs(df_sensitivities).T.nlargest(5,0)
             Factor_sens = float(df_sensitivities[factor])
 
