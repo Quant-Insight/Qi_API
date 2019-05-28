dfb0222fac2006a178588783f239ebaf231702ef
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2019-05-28 10:03:18 +0100

    Create Get_Tags.py

---
+++ Main_API_functions/Get_Tags.py
@@ -0,0 +1,46 @@
+    
+#######################################################################################################################################
+# 
+# get_tags() is a QI API function to retrieve all the tags available. 
+# 
+# Inputs:
+#
+#
+# Output: All the information of the requested model.
+#               * e.g.
+#                      [{'label': 'STOXX Europe 600'},
+#                       {'label': 'S&P 1500'},
+#                       {'label': ...},
+#                       {'label': ...},
+#                       {'label': ...},
+#                       ...
+#                     ]
+#
+#######################################################################################################################################
+
+
+from __future__ import print_function
+import time
+import qi_client
+from qi_client.rest import ApiException
+from pprint import pprint
+
+# Configure API key authorization: QI API Key
+configuration = qi_client.Configuration()
+
+# Add the API Key provided by QI
+configuration.api_key['X-API-KEY'] = 'YOUR_API_KEY'
+
+# Uncomment to set up a proxy
+# configuration.proxy = 'http://localhost:3128'
+
+# create an instance of the API class
+api_instance = qi_client.DefaultApi(qi_client.ApiClient(configuration))
+
+try:
+    # Get list of all defined tags on the system
+    api_response = api_instance.get_tags()
+    pprint(api_response)
+    
+except ApiException as e:
+    print("Exception when calling DefaultApi->get_tags: %s\n" % e)
