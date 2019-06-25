37a806977ff428cda949f021239e99be20b6b54e
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2019-06-25 14:57:02 +0100

    Update Get_Model_Data.py

--- 2.Pull_Data/Get_Model_Data.py
+++ 2.Pull_Data/Get_Model_Data.py
@@ -31,37 +31,45 @@
 
 
 def get_model_data(model, start, end, term):
-    # Note that the periods of time can be longer than a year. We need to divide them in one-year periods.   
-    year_start = int(start[:4])
-    year_end = int(end[:4])
-    time_series = []
     
-    for year in range(year_start, year_end + 1):
-        query_start = start
-        
-        if year != year_start:
-            date_from = '%d-01-01' % year
-        else:
-            date_from = start
-        if year != year_end:
-            date_to = '%d-12-31' % year
-        else:
-            date_to = end
+    #Note that we only have data from Monday to Friday.
+    start_date = datetime.strptime(start, '%Y-%m-%d')
+    end_date = datetime.strptime(end, '%Y-%m-%d')    
     
-        time_series += api_instance.get_model_timeseries(model=model,date_from=date_from,date_to=date_to,term=term)
-    
-    FVG = [data.sigma for data in time_series]
-    Rsq = [data.rsquare for data in time_series]
-    dates = [data._date for data in time_series]
-    
-    model_value = [data.fair_value for data in time_series]
-    percentage_gap = [data.percentage_gap for data in time_series]
-    absolute_gap = [data.absolute_gap for data in time_series]
+    if (start_date.weekday() == 5 or start_date.weekday() == 6) and (end_date.weekday() == 5 or end_date.weekday() == 6) and ((end_date - start_date).days == 1):
+        print('Please choose a period of time which includes days between Monday and Friday.')
+    else: 
+        # Note that the periods of time can be longer than a year. We need to divide them in one-year periods.   
+        year_start = int(start[:4])
+        year_end = int(end[:4])
+        time_series = []
+
+        for year in range(year_start, year_end + 1):
+            query_start = start
+
+            if year != year_start:
+                date_from = '%d-01-01' % year
+            else:
+                date_from = start
+            if year != year_end:
+                date_to = '%d-12-31' % year
+            else:
+                date_to = end
+
+            time_series += api_instance.get_model_timeseries(model=model,date_from=date_from,date_to=date_to,term=term)
+
+        FVG = [data.sigma for data in time_series]
+        Rsq = [data.rsquare for data in time_series]
+        dates = [data._date for data in time_series]
+
+        model_value = [data.fair_value for data in time_series]
+        percentage_gap = [data.percentage_gap for data in time_series]
+        absolute_gap = [data.absolute_gap for data in time_series]
 
 
-    df_ = pandas.DataFrame({'FVG':FVG, 'Rsq':Rsq, 'Model Value':model_value, 'Percentage Gap':percentage_gap,
-                            'Absolute Gap':absolute_gap})
-    
-    df_.index = dates
-    
-    return df_
+        df_ = pandas.DataFrame({'FVG':FVG, 'Rsq':Rsq, 'Model Value':model_value, 'Percentage Gap':percentage_gap,
+                                'Absolute Gap':absolute_gap})
+
+        df_.index = dates
+
+        return df_
