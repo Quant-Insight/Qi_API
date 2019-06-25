a7c44468e055e1d7489a785ee1261d1f3feda54a
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2019-06-25 15:39:45 +0100

    Update Get_Rsq.py

--- 2.Pull_Data/Get_Rsq.py
+++ 2.Pull_Data/Get_Rsq.py
@@ -28,31 +28,39 @@
 
 
 def get_Rsq(model, start, end, term):
-# Note that this may be more than 1 year of data, so need to split requests
-    year_start = int(start[:4])
-    year_end = int(end[:4])
-    time_series = []
     
-    for year in range(year_start, year_end + 1):
-        query_start = start
-        if year != year_start:
-            date_from = '%d-01-01' % year
-        else:
-            date_from = start
-        if year != year_end:
-            date_to = '%d-12-31' % year
-        else:
-            date_to = end
+    #Note that we only have data from Monday to Friday.
+    start_date = datetime.strptime(start, '%Y-%m-%d')
+    end_date = datetime.strptime(end, '%Y-%m-%d')    
     
-        time_series += api_instance.get_model_timeseries(
-        model,
-        date_from=date_from,
-        date_to=date_to,
-        term=term)
-        
-    rsq = [data.rsquare for data in time_series]
-    dates = [data._date for data in time_series]
-    df = pandas.DataFrame({'Dates': dates, 'Rsq': rsq})
-    df.set_index('Dates', inplace=True)
+    if (start_date.weekday() == 5 or start_date.weekday() == 6) and (end_date.weekday() == 5 or end_date.weekday() == 6) and ((end_date - start_date).days == 1):
+        print('Please choose a period of time which includes days between Monday and Friday.')
+    else: 
+        # Note that this may be more than 1 year of data, so need to split requests
+        year_start = int(start[:4])
+        year_end = int(end[:4])
+        time_series = []
     
-    return df
+        for year in range(year_start, year_end + 1):
+            query_start = start
+            if year != year_start:
+                date_from = '%d-01-01' % year
+            else:
+                date_from = start
+            if year != year_end:
+                date_to = '%d-12-31' % year
+            else:
+                date_to = end
+
+            time_series += api_instance.get_model_timeseries(
+            model,
+            date_from=date_from,
+            date_to=date_to,
+            term=term)
+
+        rsq = [data.rsquare for data in time_series]
+        dates = [data._date for data in time_series]
+        df = pandas.DataFrame({'Dates': dates, 'Rsq': rsq})
+        df.set_index('Dates', inplace=True)
+
+        return df
