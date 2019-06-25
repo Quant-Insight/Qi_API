62ffb7c7841f790793a99ca3847aa3130ece94cf
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2019-06-25 09:13:06 +0100

    Update Top_RSq_Changes.py

--- 3.Use_Cases/Top_RSq_Changes.py
+++ 3.Use_Cases/Top_RSq_Changes.py
@@ -2,12 +2,24 @@ def Top_RSq_Changes(models, number):
 
     from datetime import datetime, timedelta
     import pandas
-
-    date_datetime = datetime.now().date() - timedelta(days=1)
+    
+    # Data is just available from Monday to Friday. We need to check that the dates we are choosing are between those days. 
+    if datetime.today().weekday() == 0:
+        date = datetime.strftime(datetime.now() - timedelta(3), '%Y-%m-%d')
+    elif datetime.today().weekday() == 6:
+        date = datetime.strftime(datetime.now() - timedelta(2), '%Y-%m-%d')
+    else:
+        date = datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d')
+    
     date_1m_datetime = datetime.now().date() - timedelta(days=30)
     
-    date = datetime.strftime(date_datetime,'%Y-%m-%d')
-    date_1m = datetime.strftime(date_1m_datetime,'%Y-%m-%d')
+    if date_1m_datetime.weekday() == 0:
+        date_1m = datetime.strftime(date_1m_datetime - timedelta(3), '%Y-%m-%d')
+    elif date_1m_datetime.weekday() == 6:
+        date_1m = datetime.strftime(date_1m_datetime - timedelta(2), '%Y-%m-%d')
+    else:
+        date_1m = datetime.strftime(date_1m_datetime - timedelta(1), '%Y-%m-%d')
+        
 
     Rsq_s = []
     Rsq_1m = []
@@ -17,7 +29,7 @@ def Top_RSq_Changes(models, number):
 
         rsq_now = get_rsq(asset,date,date,'Long Term')
         rsq_past = get_rsq(asset,date_1m,date_1m,'Long Term')
-    
+
         if (len(rsq_now) > 0) and (len(rsq_past) > 0):
             
             Rsq_s.append(float(rsq_now['Rsq']))
@@ -26,6 +38,7 @@ def Top_RSq_Changes(models, number):
             name = api_instance.get_model(asset).security_name
             asset_names.append(asset)
 
+        
     df_RSq = pandas.DataFrame({'Name':asset_names,'RSq':Rsq_s,'RSq - 1M':Rsq_1m,
                                'Change':[x-y for x,y in zip(Rsq_s,Rsq_1m)]})
     
