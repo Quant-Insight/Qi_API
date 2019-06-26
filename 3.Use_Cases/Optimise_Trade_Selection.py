c129b0c355052ec8b5dc49a8e901e5b152df820f
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2019-06-26 10:47:48 +0100

    Update Optimise_Trade_Selection.py

--- 3.Use_Cases/Optimise_Trade_Selection.py
+++ 3.Use_Cases/Optimise_Trade_Selection.py
@@ -36,53 +36,60 @@
 
 
 def optimise_trade_selection(factors,universe_asset_classes,size,date,term, **kwargs):
-
-    FACTOR_sensitivity = []
-    names = []
-    POSITION = []
-
-    universe_tag = kwargs.get('universe_tag', None)
     
-    if len(factors) > 3:
-        print('The number of factors need to be less than 3.')
-    else:   
- 
-        if (universe_tag is not None):
-            models = [x.name for x in api_instance.get_models(asset_classes = universe_asset_classes, tags=universe_tag)][::2]
-        else:
-            models = [x.name for x in api_instance.get_models(asset_classes = universe_asset_classes)][::2]
-
-        df_result = pandas.DataFrame(columns = factors + ['Total Sensitivity (Abs)'])
+    date_formated = datetime.strptime(date, '%Y-%m-%d')
+    
+    if (date_formated.weekday() == 5 or date_formated.weekday() == 6):
+        print('Please choose a day between Monday and Friday.')
         
-        print('Gathering data for all the models in the universe (it can take a while depending on the universe chosen)')
+    else:
+        
+        FACTOR_sensitivity = []
+        names = []
+        POSITION = []
 
-        for asset in models:
-            factor_sensitivities = []
-            sensitivity = api_instance.get_model_sensitivities(model=asset,date_from=date,date_to=date,term = term)
+        universe_tag = kwargs.get('universe_tag', None)
 
-            df_sensitivities = pandas.DataFrame()
+        if len(factors) > 3:
+            print('The number of factors need to be less than 3.')
+        else:   
 
-            if len(sensitivity) > 0:
-                date = [x for x in sensitivity][0]
-
-                for data in sensitivity[date]:
-                    df_sensitivities[str(data['driver_short_name'])]=[data['sensitivity']]
-                
-                
-                total = 0
-                for factor in factors:
-                    total += abs(df_sensitivities[factor][0])
-                    factor_sensitivities.append(df_sensitivities[factor][0])
-                
-                df_result.loc[asset] = factor_sensitivities + [total]
-
-        if len(df_result) > 0:
-            result = df_result.nlargest(size,'Total Sensitivity (Abs)')
-            return result
-        else:
-            if universe_tag is not None:
-                print('There are no models which satisfies the universe asset classes ' + universe_asset_classes + ' and the universe tag ' + universe_tag)
+            if (universe_tag is not None):
+                models = [x.name for x in api_instance.get_models(asset_classes = universe_asset_classes, tags=universe_tag)][::2]
             else:
-                print('There are no models which satisfies the universe asset classes ' + universe_asset_classes)
+                models = [x.name for x in api_instance.get_models(asset_classes = universe_asset_classes)][::2]
+
+            df_result = pandas.DataFrame(columns = factors + ['Total Sensitivity (Abs)'])
+
+            print('Gathering data for all the models in the universe (it can take a while depending on the universe chosen)')
+
+            for asset in models:
+                factor_sensitivities = []
+                sensitivity = api_instance.get_model_sensitivities(model=asset,date_from=date,date_to=date,term = term)
+
+                df_sensitivities = pandas.DataFrame()
+
+                if len(sensitivity) > 0:
+                    date = [x for x in sensitivity][0]
+
+                    for data in sensitivity[date]:
+                        df_sensitivities[str(data['driver_short_name'])]=[data['sensitivity']]
+
+
+                    total = 0
+                    for factor in factors:
+                        total += abs(df_sensitivities[factor][0])
+                        factor_sensitivities.append(df_sensitivities[factor][0])
+
+                    df_result.loc[asset] = factor_sensitivities + [total]
+
+            if len(df_result) > 0:
+                result = df_result.nlargest(size,'Total Sensitivity (Abs)')
+                return result
+            else:
+                if universe_tag is not None:
+                    print('There are no models which satisfies the universe asset classes ' + universe_asset_classes + ' and the universe tag ' + universe_tag)
+                else:
+                    print('There are no models which satisfies the universe asset classes ' + universe_asset_classes)
 
         
