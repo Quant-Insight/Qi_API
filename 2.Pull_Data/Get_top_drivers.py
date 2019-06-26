9a89ebc67f3425acb4c3fe41a4d6688c09a057e8
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2019-06-26 10:12:41 +0100

    Update Get_top_drivers.py

--- 2.Pull_Data/Get_top_drivers.py
+++ 2.Pull_Data/Get_top_drivers.py
@@ -30,18 +30,24 @@
 #
 #######################################################################################################################################
 
-
 def get_top_drivers(model,number,date,term):
-
-    sensitivity = api_instance.get_model_sensitivities(model=model,date_from=date,date_to=date,term=term)
-    date = [x for x in sensitivity][0]
-    df_sensitivities = pandas.DataFrame()
- 
-    for data in sensitivity[date]:
-        df_sensitivities[str(data['driver_short_name'])]=[data['sensitivity']]
-
-    top_names = abs(df_sensitivities.T).nlargest(number,0).index
-    top = df_sensitivities[top_names]
-    top = top.T.rename(columns={0:model})
     
-    return top
+    date_formated = datetime.strptime(date, '%Y-%m-%d')
+    
+    if (date_formated.weekday() == 5 or date_formated.weekday() == 6):
+        print('Please choose a date between Monday and Friday.')
+        
+    else:
+
+        sensitivity = api_instance.get_model_sensitivities(model=model,date_from=date,date_to=date,term=term)
+        date = [x for x in sensitivity][0]
+        df_sensitivities = pandas.DataFrame()
+
+        for data in sensitivity[date]:
+            df_sensitivities[str(data['driver_short_name'])]=[data['sensitivity']]
+
+        top_names = abs(df_sensitivities.T).nlargest(number,0).index
+        top = df_sensitivities[top_names]
+        top = top.T.rename(columns={0:model})
+
+        return top
