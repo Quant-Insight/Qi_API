53cd35ba5977e51ee4e76282dce2013e98bb3f81
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2019-06-26 10:53:31 +0100

    Update Portfolio_%_Exposures.py

--- 3.Use_Cases/Portfolio_%_Exposures.py
+++ 3.Use_Cases/Portfolio_%_Exposures.py
@@ -54,41 +54,48 @@
 
 
 def get_portfolio_sens_exposures_bucket(portfolio,date):
-
-    stock_names = portfolio['Name']
-    df_tot = pandas.DataFrame()
-
-    for stock in stock_names:
-
-        sensitivity = api_instance.get_model_sensitivities(model=stock,date_from=date,date_to=date,term='Long Term')
-        df_sensitivities = pandas.DataFrame()
-        date = [x for x in sensitivity.keys()][0]
-
+    
+    date_formated = datetime.strptime(date, '%Y-%m-%d')
+    
+    if (date_formated.weekday() == 5 or date_formated.weekday() == 6):
+        print('Please choose a day between Monday and Friday.')
         
-        for data in sensitivity[date]:
+    else:
 
-            if data['bucket_name'] in df_sensitivities.columns:
-                df_sensitivities[str(data['bucket_name'])][0] = df_sensitivities[str(data['bucket_name'])][0] + [data['sensitivity']]
+        stock_names = portfolio['Name']
+        df_tot = pandas.DataFrame()
 
+        for stock in stock_names:
+
+            sensitivity = api_instance.get_model_sensitivities(model=stock,date_from=date,date_to=date,term='Long Term')
+            df_sensitivities = pandas.DataFrame()
+            date = [x for x in sensitivity.keys()][0]
+
+
+            for data in sensitivity[date]:
+
+                if data['bucket_name'] in df_sensitivities.columns:
+                    df_sensitivities[str(data['bucket_name'])][0] = df_sensitivities[str(data['bucket_name'])][0] + [data['sensitivity']]
+
+                else:
+                    df_sensitivities[str(data['bucket_name'])]=[data['sensitivity']]
+
+            df_sensitivities = df_sensitivities.rename(index={0:stock})
+            df_sensitivities = df_sensitivities.sort_index(axis=1)
+            if df_tot.empty:
+                df_tot = df_sensitivities
             else:
-                df_sensitivities[str(data['bucket_name'])]=[data['sensitivity']]
+                df_tot = df_tot.append(df_sensitivities)
 
-        df_sensitivities = df_sensitivities.rename(index={0:stock})
-        df_sensitivities = df_sensitivities.sort_index(axis=1)
-        if df_tot.empty:
-            df_tot = df_sensitivities
-        else:
-            df_tot = df_tot.append(df_sensitivities)
-            
-            
-    portfolio_sensitivities = pandas.DataFrame({},columns = df_tot.columns)
-    
-    for stock in stock_names:
 
-        portfolio_sensitivities.loc[stock] = [a*float(portfolio[portfolio['Name']==stock]['Weight'])*float(portfolio[portfolio['Name']==stock]['L/S']) for a in df_tot.loc[stock]]
+        portfolio_sensitivities = pandas.DataFrame({},columns = df_tot.columns)
 
-    portfolio_sensitivities.loc['Total'] = [portfolio_sensitivities[x].sum() for x in portfolio_sensitivities.columns]
-    
-    portfolio_exposures = portfolio_sensitivities.loc[['Total']]
-    
-    return portfolio_sensitivities
+        for stock in stock_names:
+
+            portfolio_sensitivities.loc[stock] = [a*float(portfolio[portfolio['Name']==stock]['Weight'])*float(portfolio[portfolio['Name']==stock]['L/S']) for a in df_tot.loc[stock]]
+
+        portfolio_sensitivities.loc['Total'] = [portfolio_sensitivities[x].sum() for x in portfolio_sensitivities.columns]
+
+        portfolio_exposures = portfolio_sensitivities.loc[['Total']]
+
+        return portfolio_sensitivities
