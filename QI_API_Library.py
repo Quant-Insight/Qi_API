b7e75b82231a892188800402042be99daefa4f5e
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2019-11-06 16:58:33 +0000

    Update QI_API_Library.py

--- QI_API_Library.py
+++ QI_API_Library.py
@@ -108,7 +108,72 @@ def get_bucket_drivers(model,date,term):
 
     return df_sensitivities
 
+def get_portfolio_sens_exposures_factor(portfolio,date):
 
+    stock_names = portfolio['Name']
+    df_tot = pandas.DataFrame()
+
+    for stock in stock_names:
+
+        sensitivity = api_instance.get_model_sensitivities(model=stock,date_from=date,date_to=date, term='Long Term')
+        df_sensitivities = pandas.DataFrame()
+        date = [x for x in sensitivity.keys()][0]
+
+        for data in sensitivity[date]:
+            df_sensitivities[str(data['driver_short_name'])]=[data['sensitivity']]
+
+        df_sensitivities = df_sensitivities.rename(index={0:stock})
+        df_sensitivities = df_sensitivities.sort_index(axis=1)
+        if df_tot.empty:
+            df_tot = df_sensitivities
+        else:
+            df_tot = df_tot.append(df_sensitivities)
+
+    portfolio_sensitivities = pandas.DataFrame({},columns = df_tot.columns)
+    
+    for stock in stock_names:
+
+        portfolio_sensitivities.loc[stock] = [a*float(portfolio[portfolio['Name']==stock]['Weight'])*float(portfolio[portfolio['Name']==stock]['L/S']) for a in df_tot.loc[stock]]
+
+    portfolio_sensitivities.loc['Total'] = [portfolio_sensitivities[x].sum() for x in portfolio_sensitivities.columns]
+    
+    portfolio_exposures = portfolio_sensitivities.loc[['Total']]
+    
+    return portfolio_sensitivities
+
+
+def get_portfolio_cash_exposures_factor(portfolio,date):
+
+    stock_names = portfolio['Name']
+    df_tot = pandas.DataFrame()
+
+    for stock in stock_names:
+
+        sensitivity = api_instance.get_model_sensitivities(model=stock,date_from=date,date_to=date, term='Long Term')
+        df_sensitivities = pandas.DataFrame()
+        date = [x for x in sensitivity.keys()][0]
+
+        for data in sensitivity[date]:
+            df_sensitivities[str(data['driver_short_name'])]=[data['sensitivity']]
+
+        df_sensitivities = df_sensitivities.rename(index={0:stock})
+        df_sensitivities = df_sensitivities.sort_index(axis=1)
+        if df_tot.empty:
+            df_tot = df_sensitivities
+        else:
+            df_tot = df_tot.append(df_sensitivities)
+
+    portfolio_sensitivities = pandas.DataFrame({},columns = df_tot.columns)
+    
+    for stock in stock_names:
+
+        portfolio_sensitivities.loc[stock] = [a*float(portfolio[portfolio['Name']==stock]['Position'])/100 for a in df_tot.loc[stock]]
+
+    portfolio_sensitivities.loc['Total'] = [portfolio_sensitivities[x].sum() for x in portfolio_sensitivities.columns]
+    
+    portfolio_exposures = portfolio_sensitivities.loc[['Total']]
+    
+    return portfolio_sensitivities
 
 
 def get_portfolio_sens_exposures_bucket(portfolio,date):
