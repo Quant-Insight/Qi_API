72361221c4ab470fe8aa0e0b6774ac1cfbed1c57
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2019-11-26 16:39:21 +0000

    Create Get_Model_Names_From_Tickers.py

---
+++ 3.Use_Cases/Get_Model_Names_From_Tickers.py
@@ -0,0 +1,50 @@
+def get_model_names_from_tickers(tickers):
+
+    ### Load US Equities from API
+
+    API_US = [x.name for x in api_instance.get_models(asset_classes='Equity',tags='USD')][::2]
+
+
+    ### Load other Equities from API
+
+    API_other = [x.name for x in api_instance.get_models(asset_classes='Equity')][::2]
+    [API_other.remove(model) for model in API_US]
+
+
+    model_names = []
+
+    for ticker in tickers:
+
+        ### Check US tickers
+
+        if any([x in ticker for x in [' US ',' UN ',' UW ']]):
+
+            temp_model = ticker.split(' ')[0]
+
+            if temp_model in API_US:
+                model_names.append(temp_model)
+
+            elif temp_model + ' US' in API_US:
+                model_names.append(temp_model + ' US')
+
+            else:
+                model_names.append(None)
+
+
+
+        ### Check other tickers
+
+        else:
+
+            potential_models = [model for model in API_other if model.split(' ')[0] in [x.split(' ')[0] for x in tickers]]
+            potential_tickers = [api_instance.get_model(model).definition.instrument1.ticker for model in potential_models]
+
+            if ticker in potential_tickers:
+                idx = potential_tickers.index(ticker)
+                model_names.append(potential_models[idx])
+
+            else:
+
+                model_names.append(None)
+                
+    return pandas.DataFrame(model_names,tickers, columns = ['Qi Model Name'])
