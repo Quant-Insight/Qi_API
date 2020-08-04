0af54a792863e90bd7622a36cf32904b8a6481b6
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2020-08-04 11:40:23 +0100

    Update Qi_wrapper.py

--- Qi_wrapper.py
+++ Qi_wrapper.py
@@ -602,15 +602,15 @@ def get_factor_stdevs(model,date):
 
 def get_model_names_from_tickers(tickers):
 
-    ### Load US Equities from API
+    ### Load US Models from API
 
-    API_US = [x.name for x in api_instance.get_models(asset_classes='Equity',tags='USD')][::2]
+    API_US = [x.name for x in api_instance.get_models(tags='USD')][::2]
 
 
-    ### Load other Equities from API
+    ### Load other Models from API
 
-    API_other = [x.name for x in api_instance.get_models(asset_classes='Equity')][::2]
-    [API_other.remove(model) for model in API_US]
+    API_other = [x.name for x in api_instance.get_models()][::2]
+    [API_other.remove(model) for model in API_US if model in API_other]
     
     potential_models = [model for model in API_other if model.split(' ')[0] in [x.split(' ')[0] for x in tickers]]
     potential_tickers = [api_instance.get_model(model).definition.instrument1.ticker for model in potential_models]
@@ -621,7 +621,7 @@ def get_model_names_from_tickers(tickers):
 
         ### Check US tickers
 
-        if any([x in ticker for x in [' US',' UN',' UW']]):
+        if any([x in ticker for x in [' US ',' UN ',' UW ']]):
 
             temp_model = ticker.split(' ')[0]
 
@@ -630,6 +630,10 @@ def get_model_names_from_tickers(tickers):
 
             elif temp_model + ' US' in API_US:
                 model_names.append(temp_model + ' US')
+                
+            elif ticker in potential_tickers:
+                idx = potential_tickers.index(ticker)
+                model_names.append(potential_models[idx])
 
             else:
                 model_names.append(None)
@@ -639,9 +643,21 @@ def get_model_names_from_tickers(tickers):
         ### Check other tickers
 
         else:
+            
+            ### Adjust German & Japan Tickers
+            
+            if ' GR ' in ticker:
+                temp_ticker = ticker.replace('GR','GY')
+                
+            elif ' JP ' in ticker:
+                temp_ticker = ticker.replace('JP','JT')
+                
+            else:
+                temp_ticker = ticker
 
-            if ticker in potential_tickers:
-                idx = potential_tickers.index(ticker)
+
+            if temp_ticker in potential_tickers:
+                idx = potential_tickers.index(temp_ticker)
                 model_names.append(potential_models[idx])
 
             else:
