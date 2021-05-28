9d6e79aad171b8e6c4c77cc014b0a1f724fbaf27
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2021-05-28 12:59:54 +0100

    Update FVG_Back_Test.py

--- 3.Use_Cases/FVG_Back_Test.py
+++ 3.Use_Cases/FVG_Back_Test.py
@@ -12,7 +12,7 @@
 #           * price_data - a dataframe of your asset price data,
 #             must be in the following format:
 #
-#                   |            | AAPL US Equity | A US Equity |
+#                   |            |     AAPL       |     A       |
 #                   | 2019-01-02 |     ##.##      |   ##.##     |
 #                   | 2019-01-03 |     ##.##      |   ##.##     |
 #                   | 2019-01-04 |     ##.##      |   ##.##     |
@@ -20,7 +20,7 @@
 #             The dates must be the index of the dataframe, and have the format 'YYYY-MM-DD'                  
 #               
 #
-#            * assets - a list of the assets you want to back-test e.g. ['AAPL US Equity,'A US Equity']
+#            * assets - a list of the assets you want to back-test e.g. ['AAPL,'A']
 #            * open_arg - the absolute value the FVG needs to be to open a trade e.g. 1.0
 #            * close_arg - the absolute value the FVG needs to be to close a trade e.g. 0.25
 #            * rsq_arg - the value the RSq needs to be to open a trade e.g. 65
@@ -30,7 +30,7 @@
 #            * term - chose between 'Short Term' or 'Long Term' Qi data
 #
 #
-# Example of call: fvg_back_test(['AAPL US Equity','A US Equity'],price_data,1.0,0.25,65,['Long','Short'],'2009-01-02','2019-01-04','Long Term')
+# Example of call: fvg_back_test(['AAPL','A'],price_data,1.0,0.25,65,['Long','Short'],'2009-01-02','2019-01-04','Long Term')
 # 
 # Output: 
 #
@@ -60,90 +60,78 @@ def fvg_back_test(assets,price_data,open_arg,close_arg,rsq_arg,l_s,start_date,en
     trade_FVG = []
     CloseDate = []
     
-    # Convert tickers to Qi model names 
-    model_names = Qi_wrapper.get_model_names_from_tickers(assets)
-    
-    for asset in assets:    
+    for model_name in assets:    
 
-        model_name = model_names['Qi Model Name'][asset]
-        
-        # If no Qi model matches a ticker
-        if model_name == None:
-            print(asset + ': Ticker not found in any Qi Model')
-            
-        # Otherwise continue to test FVGs
-        else:
-        
-            # Pull model data from API and price data from your csv
-            data = Qi_wrapper.get_model_data(model_name,start_date,end_date,term)
-            price = price_data[asset]
-            
-            # Find common dates between the two data sets, and only consider those
-            common_dates = [date for date in data.index if date in price.index]
-            data = data.loc[common_dates]
-            price = price.loc[common_dates]
-            
-            FVG = data['FVG']
-            RSq = data['Rsq']
+        # Pull model data from API and price data from your csv
+        data = Qi_wrapper.get_model_data(model_name,start_date,end_date,term)
+        price = price_data[model_name]
 
-            # Find potential times to close a trade
-            FVG_closelong = FVG>-1*close_arg
-            FVG_closeshort = FVG<close_arg
+        # Find common dates between the two data sets, and only consider those
+        common_dates = [date for date in data.index if date in price.index]
+        data = data.loc[common_dates]
+        price = price.loc[common_dates]
+
+        FVG = data['FVG']
+        RSq = data['Rsq']
+
+        # Find potential times to close a trade
+        FVG_closelong = FVG>-1*close_arg
+        FVG_closeshort = FVG<close_arg
 
 
-            i = 0
+        i = 0
 
-            while i < (len(FVG)):
+        while i < (len(FVG)):
 
-                # Longs
-                if 'Long' in l_s and FVG[i] < -1*open_arg and (FVG_closelong[i:] == True).sum() > 0 and RSq[i] > rsq_arg:
+            # Longs
+            if 'Long' in l_s and FVG[i] < -1*open_arg and (FVG_closelong[i:] == True).sum() > 0 and RSq[i] > rsq_arg:
 
-                    # Find open & close dates for the trade
-                    new_price_index = FVG_closelong.tolist().index(True,i,len(FVG))
-                    close_date = str(FVG.index[new_price_index])[:10]
-                    open_date = str(FVG.index[i])[:10]
+                # Find open & close dates for the trade
+                new_price_index = FVG_closelong.tolist().index(True,i,len(FVG))
+                close_date = str(FVG.index[new_price_index])[:10]
+                open_date = str(FVG.index[i])[:10]
 
-                    # Calculate returns & holding period
-                    returns = ((price[close_date] - price[open_date])/price[open_date])*100   
-                    TradeLength = new_price_index - i
-                    Holding_Times.append(TradeLength)
+                # Calculate returns & holding period
+                returns = ((price[close_date] - price[open_date])/price[open_date])*100   
+                TradeLength = new_price_index - i
+                Holding_Times.append(TradeLength)
 
-                    # Add trade info to results
-                    Name.append(asset)
-                    Date.append(open_date)   
-                    ALL_returns.append(returns)
-                    LongShort.append('Long')
-                    CloseDate.append(close_date)
-                    trade_RSq.append(RSq[i])
-                    trade_FVG.append(FVG[i])
+                # Add trade info to results
+                Name.append(model_name)
+                Date.append(open_date)   
+                ALL_returns.append(returns)
+                LongShort.append('Long')
+                CloseDate.append(close_date)
+                trade_RSq.append(RSq[i])
+                trade_FVG.append(FVG[i])
 
-                    # Only open one trade in any asset at one time, so skip to end of trade
-                    i = new_price_index + 1
+                # Only open one trade in any asset at one time, so skip to end of trade
+                i = new_price_index + 1
 
-                    
-                # Shorts
-                elif 'Short' in l_s and FVG[i] > open_arg and (FVG_closeshort[i:] == True).sum() > 0 and RSq[i] > rsq_arg:
 
-                    new_price_index = FVG_closeshort.tolist().index(True,i,len(FVG))
-                    close_date = str(FVG.index[new_price_index])[:10]
-                    open_date = str(FVG.index[i])[:10]
+            # Shorts
+            elif 'Short' in l_s and FVG[i] > open_arg and (FVG_closeshort[i:] == True).sum() > 0 and RSq[i] > rsq_arg:
 
-                    returns = (-(price[close_date] - price[open_date])/price[open_date])*100   
-                    TradeLength = new_price_index - i
-                    Holding_Times.append(TradeLength)
+                new_price_index = FVG_closeshort.tolist().index(True,i,len(FVG))
+                close_date = str(FVG.index[new_price_index])[:10]
+                open_date = str(FVG.index[i])[:10]
 
-                    Name.append(asset)
-                    Date.append(open_date)   
-                    ALL_returns.append(returns)
-                    LongShort.append('Short')
-                    CloseDate.append(close_date)
-                    trade_RSq.append(RSq[i])
-                    trade_FVG.append(FVG[i])
+                returns = (-(price[close_date] - price[open_date])/price[open_date])*100   
+                TradeLength = new_price_index - i
+                Holding_Times.append(TradeLength)
 
-                    i = new_price_index + 1
+                Name.append(model_name)
+                Date.append(open_date)   
+                ALL_returns.append(returns)
+                LongShort.append('Short')
+                CloseDate.append(close_date)
+                trade_RSq.append(RSq[i])
+                trade_FVG.append(FVG[i])
 
-                else:            
-                    i = i + 1
+                i = new_price_index + 1
+
+            else:            
+                i = i + 1
 
     
     # Calculate results metrics
@@ -195,78 +183,67 @@ def FVG_Back_Test_rates(assets,price_data,open_arg,close_arg,RSq_arg,start_date,
     LongShort = []
     RSQ = []
     CloseDate = []
+
     
-    # Convert tickers to Qi model names 
-    model_names = get_model_names_from_tickers(assets)
-    
-    for asset in assets:    
-        
-        model_name = model_names['Qi Model Name'][asset]
-        
-        # If no Qi model matches a ticker
-        if model_name == None:
-            print(asset + ': Ticker not found in any Qi Model')
+    for model_name in assets:    
             
-        # Otherwise continue to test FVGs
-        else:
-            
-            # Pull model data from API and price data from your csv
-            data = get_model_data(model_name,start_date,end_date,term)
-            price = price_data[asset]
+        # Pull model data from API and price data from your csv
+        data = get_model_data(model_name,start_date,end_date,term)
+        price = price_data[model_name]
 
-            # Find common dates between the two data sets, and only consider those
-            common_dates = [date for date in data.index if date in price.index]
-            data = data.loc[common_dates]
-            price = price.loc[common_dates]
+        # Find common dates between the two data sets, and only consider those
+        common_dates = [date for date in data.index if date in price.index]
+        data = data.loc[common_dates]
+        price = price.loc[common_dates]
 
-            FVG = data['FVG']
-            Rsq = data['Rsq']
+        FVG = data['FVG']
+        Rsq = data['Rsq']
 
-            # Find potential times to close a trade
-            FVG_closelong = FVG>-1*close_arg
-            FVG_closeshort = FVG<close_arg
+        # Find potential times to close a trade
+        FVG_closelong = FVG>-1*close_arg
+        FVG_closeshort = FVG<close_arg
 
-            i = 0
+        i = 0
 
-            while i < (len(FVG)):
+        while i < (len(FVG)):
 
-                # Longs
-                if 'Long' in S_L and FVG[i] < -1*open_arg and (FVG_closelong[i:] == True).sum() > 0 and Rsq[i] > RSq_arg:
+            # Longs
+            if 'Long' in S_L and FVG[i] < -1*open_arg and (FVG_closelong[i:] == True).sum() > 0 and Rsq[i] > RSq_arg:
 
-                    # Find open & close dates for the trade
-                    new_price_index = FVG_closelong.tolist().index(True,i,len(FVG))
-                    returns = (price[new_price_index] - price[i])   
-                    TradeLength = new_price_index - i
-                    Holding_Times.append(TradeLength)
-                    Name.append(asset)
-                    Date.append(FVG.index[i])   
-                    ALL_returns.append(returns)
-                    LongShort.append('Long')
-                    CloseDate.append(FVG.index[new_price_index])
-                    RSQ.append(Rsq[i])
+                # Find open & close dates for the trade
+                new_price_index = FVG_closelong.tolist().index(True,i,len(FVG))
+                returns = (price[new_price_index] - price[i])   
+                TradeLength = new_price_index - i
+                Holding_Times.append(TradeLength)
+                Name.append(model_name)
+                Date.append(FVG.index[i])   
+                ALL_returns.append(returns)
+                LongShort.append('Long')
+                CloseDate.append(FVG.index[new_price_index])
+                RSQ.append(Rsq[i])
 
-                    # Only open one trade in any asset at one time, so skip to end of trade
-                    i = new_price_index + 1
+                # Only open one trade in any asset at one time, so skip to end of trade
+                i = new_price_index + 1
 
-                # Shorts
-                elif 'Short' in S_L and FVG[i] > open_arg and (FVG_closeshort[i:] == True).sum() > 0 and Rsq[i] > RSq_arg:
+            # Shorts
+            elif 'Short' in S_L and FVG[i] > open_arg and (FVG_closeshort[i:] == True).sum() > 0 and Rsq[i] > RSq_arg:
 
-                    new_price_index = FVG_closeshort.tolist().index(True,i,len(FVG))
-                    returns = (-price[new_price_index] + price[i])   
-                    TradeLength = new_price_index - i
-                    Holding_Times.append(TradeLength)
+                new_price_index = FVG_closeshort.tolist().index(True,i,len(FVG))
+                returns = (-price[new_price_index] + price[i])   
+                TradeLength = new_price_index - i
+                Holding_Times.append(TradeLength)
 
-                    Name.append(asset)
-                    Date.append(FVG.index[i])   
-                    ALL_returns.append(returns)
-                    LongShort.append('Short')
-                    CloseDate.append(FVG.index[new_price_index])
-                    RSQ.append(Rsq[i])
+                Name.append(model_name)
+                Date.append(FVG.index[i])   
+                ALL_returns.append(returns)
+                LongShort.append('Short')
+                CloseDate.append(FVG.index[new_price_index])
+                RSQ.append(Rsq[i])
 
-                    i = new_price_index + 1
+                i = new_price_index + 1
 
-                else:            
-                    i = i + 1
+            else:            
+                i = i + 1
     
     # Calculate results metrics
     HitRate = 100*sum([x > 0 for x in ALL_returns])/len(ALL_returns)
