92e1aabd21a7567632af7c7307b778fab504c0e3
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2023-01-10 10:01:05 +0000

    Update Get_Bucket_Grid.py
    Replace append pandas' function with concat.

--- 2.Pull_Data/Get_Bucket_Grid.py
+++ 2.Pull_Data/Get_Bucket_Grid.py
@@ -65,6 +65,6 @@ def get_bucket_grid(model,start,end,term):
             if sensitivity_grid.empty:
                 sensitivity_grid = df_sensitivities
             else:
-                sensitivity_grid = sensitivity_grid.append(df_sensitivities)
+                sensitivity_grid = pandas.concat([sensitivity_grid, df_sensitivities], axis = 0, join = 'outer')
 
         return sensitivity_grid
