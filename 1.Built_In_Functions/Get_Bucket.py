4c9a5e5a7a76229bfe7e8dc5878b279bcc2d033d
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2023-10-11 08:07:06 +0000

    Update Get_Bucket.py

--- 1.Built_In_Functions/Get_Bucket.py
+++ 1.Built_In_Functions/Get_Bucket.py
@@ -4,12 +4,41 @@
 # get_bucket(bucket, numeric_id) is a QI API function to retrieve the id and the name of a given bucket from our API.  
 # 
 # Inputs:
-#               * bucket - numeric id or name of the bucket (e.g. 'Corp credit')
+#               * bucket - numeric id or name of the bucket (e.g. 'Corporate Credit')
 #               * numeric_id (optional) - True to consider bucket as a numeric id. False to consider bucket as a name (False by default).
 #
 # Output: the id and the name of the bucket requested.
 #               * e.g.
-#                      {'id': 23, 'name': 'Corp credit'}
+#                      {'drivers': [{'buckets': ['Corporate Credit'],
+#                      'deactivated': None,
+#                      'expression': 'a',
+#                      'id': 1,
+#                      'name': 'FinSub Credit [{FinSub Credit | bid_spread}]',
+#                      'short_name': 'FinSub Credit',
+#                      'status': 'ACTIVE',
+#                      'timeseries_ety1': {'field': 'bid_spread',
+#                                          'id': 3736,
+#                                          'instrument': {'asset_class': None,
+#                                                         'id': 4150,
+#                                                         'identifiers': {'BloombergTicker': 'ITRXEUE '
+#                                                                                            'CBGT '
+#                                                                                            'Index',
+#                                                                         'IHSID': 'FinSub '
+#                                                                                  'credit'},
+#                                                         'is_future': False,
+#                                                         'name': 'FinSub credit',
+#                                                         'source': 'Ihsmarkit',
+#                                                         'status': 'active'},
+#                                          'last_source': 'Ihsmarkit',
+#                                          'status': 'active'},
+#                      'timeseries_ety2': None,
+#                      'timeseries_ety3': None,
+#                      'timeseries_ety4': None},
+#        ...
+#        'name': 'Corporate Credit',
+#         'public': None,
+#         'state': None,
+#         'type': 'QI'}
 #
 #######################################################################################################################################
 
