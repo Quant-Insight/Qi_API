c4c82cd9a7ab540cec0ec6c3e9b9072a4101f4bd
Quant-Insight <49234561+Quant-Insight@users.noreply.github.com>
2023-10-11 09:35:37 +0000

    Update Get_Percentage_Gap.py

--- 2.Pull_Data/Get_Percentage_Gap.py
+++ 2.Pull_Data/Get_Percentage_Gap.py
@@ -62,6 +62,5 @@ def get_Percentage_Gap(model, start, end, term):
         df_ = pandas.DataFrame({'Percentage Gap':percentage_gap})
 
         df_.index = dates
-        df_.index = df_.index.strftime('%Y-%m-%d')
 
         return df_
